02b6077eb7b2c34b85779f8dde9e06d8565f933c
manu <manuthala7@gmail.com>
2025-02-07 18:36:05 +0530

    First Commit

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+// Pascal casing
+function Message() {
+  // JSX javascript xml(babeljs.io)
+  const name = "Manu";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
+}
+
+export default Message;
